dd89842bbee3b1d93a780eadef69edf325904b80
spidey25 <shirley_sedeste@yahoo.com.sg>
2014-12-03 15:02:21 +0800

    Update 003_insert_value.sql

--- deltas/003_insert_value.sql
+++ deltas/003_insert_value.sql
@@ -1,5 +1,6 @@
-Insert into test (data, dateofbirth) values ('9','2014-12-02');
-Insert into test (data, dateofbirth) values ('10','2014-12-03');
-Insert into test (data, dateofbirth) values ('11','2014-12-04');
-Insert into test (dateofbirth) value ('2014-11-30') where id='6';
-Insert into test (dateofbirth) value ('2014-12-01') where id='7';
+Insert into test (data) values ('9');
+Insert into test (data) values ('10');
+Insert into test (data) values ('11');
+
+Update into test (dateofbirth) value ('2014-11-30') where id='6';
+Update into test (dateofbirth) value ('2014-12-01') where id='7';
