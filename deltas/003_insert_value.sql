301c607482c485998b214c3b1bb52bdb1fe7fbb2
spidey25 <shirley_sedeste@yahoo.com.sg>
2014-12-03 15:07:17 +0800

    Update 003_insert_value.sql

--- deltas/003_insert_value.sql
+++ deltas/003_insert_value.sql
@@ -1,6 +1,6 @@
-Insert into test (data) values ('9');
-Insert into test (data) values ('10');
-Insert into test (data) values ('11');
+Insert into test (id,data) values ('9','hello world');
+Insert into test (id,data) values ('10','brb');
+Insert into test (id,data) values ('11','echos');
 
 --//@UNDO
 
