924d5da2a270abf7a8f9a0e0d0ab473115699c2b
spidey25 <shirley_sedeste@yahoo.com.sg>
2014-12-03 14:49:18 +0800

    Create 003_insert_value.sql

---
+++ deltas/003_insert_value.sql
@@ -0,0 +1,5 @@
+Insert into test (data, dateofbirth) values (9,2014-12-02);
+Insert into test (data, dateofbirth) values (10,2014-12-03);
+Insert into test (data, dateofbirth) values (11,2014-12-04);
+Insert into test (dateofbirth) value (2014-11-30) where id=6;
+Insert into test (dateofbirth) value (2014-12-01) where id=7;
