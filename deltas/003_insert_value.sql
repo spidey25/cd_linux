7533aae9ca536232a23b963cbbea902ac521daa1
spidey25 <shirley_sedeste@yahoo.com.sg>
2014-12-03 15:26:27 +0800

    Update 003_insert_value.sql

--- deltas/003_insert_value.sql
+++ deltas/003_insert_value.sql
@@ -1,9 +1,8 @@
-Insert into test (id,data) values ('9','hello world');
-Insert into test (id,data) values ('10','brb');
-Insert into test (id,data) values ('11','echos');
+Insert into test (id,data) values ("9","hello world");
 
 --//@UNDO
 
-
+Insert into test (id,data) values ('10','brb');
+Insert into test (id,data) values ('11','echos');
 Update into test (dateofbirth) value ('2014-11-30') where id='6';
 Update into test (dateofbirth) value ('2014-12-01') where id='7';
